e768acef470d2ad810eff768334cedbff196c10a
Jbernardiss <joaobernardiss@gmail.com>
2023-03-22 11:19:27 -0300

    array php

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+$aExemplo = array ("ra"=180125,"serie"=>3,"aluno"=array("nome"=>"Matioli"));
+
+echo $aExemplo["ra"]."-".$aExemplo["aluno"]["nome"]."".$aExemplo["aluno"]["sobrenome"];
+
+
+$meuArray = array ("maçã","Melão","Uva");
+
+
+
+echo count ($meuArray);
+echo "<br>";
+echo sizeof($meuArray);
+
+
+?>
+
